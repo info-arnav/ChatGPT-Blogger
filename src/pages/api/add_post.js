b72ee18c1dfb1059599fc2dbd2a0d3cdcc794395
Arnav Gupta <info@arnavgupta.net>
2023-03-21 21:06:42 +0530

    Bug fixed

--- src/pages/api/add_post.js
+++ src/pages/api/add_post.js
@@ -10,7 +10,7 @@ export default async (request, response) => {
       messages: [
         {
           role: "user",
-          content: `Write a blog for a blogging website being really descriptive as to compete with the content on wikipdeia in word count greater than 1500 with the title '${req.body.prompt}', enclode the content in a htl article tag with appropriate break line tags. dont add title html or body tags. add bold and underline where necessary. and focus on any kewords you find appropriate for the topic. just give me the article without and other introductory sentence`,
+          content: `Write a blog for a blogging website being really descriptive as to compete with the content on wikipdeia in word count greater than 1500 with the title '${request.body.prompt}', enclode the content in a htl article tag with appropriate break line tags. dont add title html or body tags. add bold and underline where necessary. and focus on any kewords you find appropriate for the topic. just give me the article without and other introductory sentence`,
         },
       ],
     }),
@@ -27,7 +27,9 @@ export default async (request, response) => {
           body: JSON.stringify({
             query: `
       mutatuion {
-        insertOneArticle (data : {title:"${req.body.prompt}", description:"${
+        insertOneArticle (data : {title:"${
+          request.body.prompt
+        }", description:"${
               res.choices[0].message.content
             }", date_created:${new Date()} }) 
       }
