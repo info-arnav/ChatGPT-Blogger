633e7b63978c167b395f9fcaae1b50e4f1cf014c
Arnav Gupta <info@arnavgupta.net>
2023-03-21 22:19:28 +0530

    API updated again

--- src/pages/api/add_post.js
+++ src/pages/api/add_post.js
@@ -1,7 +1,7 @@
 export default async (request, response) => {
+  response.json({ status: true });
   await fetch("https://infinity.itsdope.in/api/request_post", {
     method: "POST",
     body: JSON.stringify(request.body),
   });
-  response.json({ status: true });
 };
