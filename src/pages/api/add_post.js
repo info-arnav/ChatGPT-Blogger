4cd7f34e1b82cdee4b4f160037605989be1ffec5
Arnav Gupta <info@arnavgupta.net>
2023-03-22 03:41:35 +0530

    testing

--- src/pages/api/add_post.js
+++ src/pages/api/add_post.js
@@ -11,7 +11,7 @@ export default async (request, response) => {
       messages: [
         {
           role: "user",
-          content: `Write a blog for a blogging website being really descriptive as to compete with the content on wikipdeia in word count greater than 1500 with the title '${request.body.prompt}', enclode the content in a htl article tag with appropriate break line tags. dont add title html or body tags. add bold and underline where necessary. and focus on any kewords you find appropriate for the topic. just give me the article without and other introductory sentence`,
+          content: `Write  a blog for a blogging website being really descriptive as to compete with the content on wikipdeia in word count greater than 1500 with the title '${request.body.prompt}', enclode the content in a htl article tag with appropriate break line tags. dont add title html or body tags. add bold and underline where necessary. and focus on any kewords you find appropriate for the topic. just give me the article without and other introductory sentence`,
         },
       ],
     }),
