4e3298ad0b3f4fbb12264235eea219e480dc0873
Arnav Gupta <info@arnavgupta.net>
2023-03-21 22:03:04 +0530

    testing 1

--- src/pages/api/add_post.js
+++ src/pages/api/add_post.js
@@ -1,7 +1,7 @@
 export default (request, response) => {
   fetch("https://infinity.itsdope.in/api/request_post", {
     method: "POST",
-    body: JSON.stringify(request.body.body),
+    body: JSON.stringify(request.body),
   });
   response.json({ status: true });
 };
