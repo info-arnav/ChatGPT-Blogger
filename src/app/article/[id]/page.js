bf9d08ee3a3b685b313054a6b611d02ed3659a86
Arnav Gupta <info@arnavgupta.net>
2023-03-22 03:26:33 +0530

    testing

--- src/app/article/[id]/page.js
+++ src/app/article/[id]/page.js
@@ -99,50 +99,15 @@ export default async function Article({ params }) {
   const articles = await getPost(params.id);
   params.id = decodeURIComponent(params.id);
   if (articles.data.articles.length == 0) {
-    await fetch("https://api.openai.com/v1/chat/completions", {
+    fetch("https://infinity.itsdope.in/api/add_post", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API}`,
       },
       body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "user",
-            content: `Write a blog for a blogging website being really descriptive as to compete with the content on wikipdeia in word count greater than 1500 with the title '${params.id}', enclode the content in a htl article tag with appropriate break line tags. dont add title html or body tags. add bold and underline where necessary. and focus on any kewords you find appropriate for the topic. just give me the article without and other introductory sentence`,
-          },
-        ],
+        prompt: prompt.toLowerCase(),
       }),
-    })
-      .then((e) => e.json())
-      .then(
-        async (res) =>
-          await fetch(process.env.GRAPHQL_STRING, {
-            method: "POST",
-            headers: {
-              apikey: process.env.GRAPHQL_API,
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              query: `
-      mutation {
-        insertOneArticle (data : {title:"${params.id.toLowerCase()}", description:"${
-                res.choices[0].message.content
-              }", date_created:"${new Date()}" }) 
-
-            {
-              _id
-              article
-              date_created
-              title
-            } 
-      }
-    `,
-            }),
-          })
-      )
-      .catch((e) => console.log("Some error while requesting article"));
+    });
   }
   return (
     <>
