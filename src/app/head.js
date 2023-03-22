9e74eb12ac8eb43afdd9d4045c3c34b1ed5f11bf
Arnav Gupta <info@arnavgupta.net>
2023-03-22 22:58:46 +0530

    ads

--- src/app/head.js
+++ src/app/head.js
@@ -1,8 +1,16 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/logo.png" />
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7188053188045336"
+        crossorigin="anonymous"
+      ></Script>
     </>
   );
 }
